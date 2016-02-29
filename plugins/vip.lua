91bf811f0f8ea506eb99ed237bbacdbabc4313c5
AliKhodadad <alikhodadad44@gmail.com>
2016-02-29 18:13:29 +0330

    Create vip.lua

---
+++ plugins/vip.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return "با باباییم چیکار داری ؟؟ 🙂🙃"
+  end
+return {
+  description = "botfather", 
+  usage = "admin name:BaBa Ali ",
+  patterns = {
+    "^اسم کسی که بهش بگه بابا",
+  },
+  run = run
+}
+end
