67379f431fcc11f8aec90544b33a5fe4a00a4315
terinux <aryangithub@gmail.com>
2016-03-14 23:56:50 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -250,7 +250,7 @@ function create_config( )
     "tg",
     "vip"
     },
-    sudo_users = {135693512},--Sudo users
+    sudo_users = {115740444},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
@@ -260,9 +260,7 @@ function create_config( )
     ‼️ this bot is made by : @creed_is_dead
    〰〰〰〰〰〰〰〰
    ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+   ߔࠀ   @aryan_daltons
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
